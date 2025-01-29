0de915953ffae10a2830d978abb4901c69046579
artificialnouveau <46301511+artificialnouveau@users.noreply.github.com>
2025-01-29 14:56:57 +0100

    Update preprocess.py

--- util/preprocess.py
+++ util/preprocess.py
@@ -175,35 +175,29 @@ def extract_5p(lm):
 
 # utils for face reconstruction
 def align_img(img, lm, lm3D, mask=None, target_size=224., rescale_factor=102.):
-    """
-    Return:
-        transparams        --numpy.array  (raw_W, raw_H, scale, tx, ty)
-        img_new            --PIL.Image  (target_size, target_size, 3)
-        lm_new             --numpy.array  (68, 2), y direction is opposite to v direction
-        mask_new           --PIL.Image  (target_size, target_size)
-    
-    Parameters:
-        img                --PIL.Image  (raw_H, raw_W, 3)
-        lm                 --numpy.array  (68, 2), y direction is opposite to v direction
-        lm3D               --numpy.array  (5, 3)
-        mask               --PIL.Image  (raw_H, raw_W, 3)
-    """
-
     w0, h0 = img.size
     if lm.shape[0] != 5:
         lm5p = extract_5p(lm)
     else:
         lm5p = lm
 
-    # calculate translation and scale factors using 5 facial landmarks and standard landmarks of a 3D face
+    # Calculate translation and scale
     t, s = POS(lm5p.transpose(), lm3D.transpose())
-    s = rescale_factor/s
 
-    # processing the image
+    # Ensure `t` is a tuple of two floats
+    if isinstance(t, np.ndarray):
+        t = tuple(float(v) for v in t.flatten()[:2])  # Take first two elements
+
+    s = rescale_factor / s
+
+    # Processing the image
     img_new, lm_new, mask_new = resize_n_crop_img(img, lm, t, s, target_size=target_size, mask=mask)
-    trans_params = np.array([w0, h0, s, t[0], t[1]])
+
+    # Ensure `t[0]` and `t[1]` are scalars before creating the array
+    trans_params = np.array([w0, h0, s, t[0], t[1]], dtype=np.float32)
 
     return trans_params, img_new, lm_new, mask_new
+
 
 # utils for face recognition model
 def estimate_norm(lm_68p, H):
